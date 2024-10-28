e36e539909c8d046bebad187ae4b072d30ce636e
pggodoy <pigodoy1@miuandes.cl>
2024-10-27 21:57:54 -0300

    initial

--- components/navigation/TabBarIcon.tsx
+++
@@ -1,8 +0,0 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { ComponentProps } from 'react';
-
-type IconProps = ComponentProps<typeof Ionicons>;
-
-export function TabBarIcon({ style, ...rest }: IconProps) {
-  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
-}
